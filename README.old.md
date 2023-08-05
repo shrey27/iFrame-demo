67879602d4e3cdcbe85b4b1c639a49a12d11b314
shrey27 <shreypandey27@gmail.com>
2023-08-05 20:34:10 +0530

    files updated

--- README.old.md
+++
@@ -1,1 +0,0 @@
-"# iFrame-demo" 
--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import { useEffectOnce } from "./hook";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  useEffectOnce(() => {
+    window.addEventListener("message", (event) => {
+      if (event.data === "<YourMessageToParentWindow>") {
+        setCounter((e) => e + 1);
+      }
+    });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h3>counter value: {counter}</h3>
+        <iframe title="frame1" src="inline.html"></iframe>
       </header>
     </div>
   );
