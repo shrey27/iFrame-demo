f31a7cc34d9595fdc19025d160fbf8f5644a96cf
shrey27 <shreypandey27@gmail.com>
2023-08-05 19:45:18 +0530

    files deleted

--- inline.js
+++
@@ -1,7 +0,0 @@
-const btn = document.getElementById("iframe-btn");
-let counterVal = 0;
-
-btn.addEventListener("click", () => {
-  const frameParentEl = parent.__counter__;
-  frameParentEl.innerText = ++counterVal;
-});
